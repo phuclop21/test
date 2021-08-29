bc32e2aedef4d6016c31f209b942a7c779144611
Phuc Ha <hoangchelsea32@gmail.com>
2021-08-29 15:30:59 +0700

    to

---
+++ Homework/ex2.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(void)
+{
+	float upper, integration = 0.0, stepSize;
+	unsigned int subInterval = 9139;
+
+	// Input 
+	printf("Enter upper limit of integration: ");
+	scanf("%f", &upper);
+
+	// Calculation 
+	// Finding step size 
+	stepSize = (upper - 0.0) / subInterval;
+
+	for (int i = 0; i <= subInterval - 1; i++) {
+		integration += stepSize * stepSize * (i * i + (i + 1) * (i + 1));
+	} // end for
+
+	printf("\nRequired value of integration is: %.3f", integration * stepSize / 2);
+}
