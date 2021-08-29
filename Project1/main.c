853cb2c5be373483de31345289e49fb4c7651923
Phuc Ha <hoangchelsea32@gmail.com>
2021-08-29 15:27:38 +0700

    today

--- Project1/main.c
+++ Project1/main.c
@@ -1,7 +1,21 @@
 #include <stdio.h>
 
-// function main begins program execution
 int main(void)
 {
-	
-} // end main
+	float upper, integration = 0.0, stepSize;
+	unsigned int subInterval = 10000;
+
+	// Input 
+	printf("Enter upper limit of integration: ");
+	scanf("%f", &upper);
+
+	// Calculation 
+	// Finding step size 
+	stepSize = (upper - 0.0) / subInterval;
+
+	for (int i = 0; i <= subInterval - 1; i++) {
+		integration += stepSize * stepSize * (i * i + (i + 1) * (i + 1));
+	} // end for
+
+	printf("\nRequired value of integration is: %.3f", integration * stepSize / 2);
+}
